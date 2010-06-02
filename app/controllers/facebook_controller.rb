37a06aa03d8f170b1cbd6a0bae09234c1ef742d5
plusjade <plusjade@gmail.com>
2010-06-02 00:27:41 -0700

    initial ajax-heavy facebook app

--- app/controllers/facebook_controller.rb
+++ app/controllers/facebook_controller.rb
@@ -1,7 +1,9 @@
 class FacebookController < ApplicationController
+  # we need this because all ajax calls are via POST.
   skip_before_filter :verify_authenticity_token
   
   
+  # init iframe application page.
   def index
     @shop = Shop.find(1)
     render :json => @shop
@@ -9,7 +11,7 @@ class FacebookController < ApplicationController
   end
 
 
-  # init page for facebook profile tab
+  # init profile tab fbml page.
   def profile
      @shop = Shop.find(1)
      render :template => 'facebook/profile.erb', :layout => false
@@ -17,28 +19,42 @@ class FacebookController < ApplicationController
   end  
 
 
-  # get shop json
-  def shop
+  # get shop json and/or all tattoos from shop
+  def shops
     render :nothing => true and return if params[:id].nil?
+    
+    if ( params[:meta] == 'tattoos' )
+      @shop = Shop.find(params[:id], :include => :tattoos)
+      render :json => @shop.tattoos.to_json(:include => { :assets => { :only => [:id, :data_file_name] } })
+      return
+    end
     
     @shop = Shop.find(params[:id])
     render :json => @shop.to_json(:include => :assets)
-    return
   end
    
-    
-  # get tattoos from shop json  
-  def tattoos
+
+
+  # get artist json and/or all tattoos from artist.
+  def artists
     render :nothing => true and return if params[:id].nil?
 
-    @shop = Shop.find(params[:id], :include => :tattoos)
-    render :json => @shop.tattoos.to_json(:include => { :assets => { :only => [:id, :data_file_name] } })
-    return
+    if ( params[:meta] == 'tattoos' )
+      @artist = Artist.find(params[:id], :include => :tattoos)
+      render :json => @artist.tattoos.to_json(:include => { :assets => { :only => [:id, :data_file_name] } })
+      return
+    end
+        
+    @artist = Artist.find(params[:id])
+    render :json => @artist.to_json(:include => :assets)
   end
+  
+      
+
 
 
   # get a single tattoo 
-  def tattoo
+  def tattoos
     render :nothing => true and return if params[:id].nil?
 
     @tattoo = Tattoo.find(params[:id])
--- config/routes.rb
+++ config/routes.rb
@@ -20,7 +20,7 @@ ActionController::Routing::Routes.draw do |map|
 
   #map.resources :facebook
 
-  map.connect 'facebook/:action/:id', :controller => 'facebook'
+  map.connect 'facebook/:action/:id/:meta', :controller => 'facebook'
   # The priority is based upon order of creation: first created -> highest priority.
 
   # Sample of regular route:
