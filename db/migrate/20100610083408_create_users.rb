04ac369b66ef2a7cf0a026bb11b3c48851c45991
plusjade <plusjade@gmail.com>
2010-06-15 23:41:18 -0700

    added some links for basic admin stuff

--- db/migrate/20100610083408_create_users.rb
+++ db/migrate/20100610083408_create_users.rb
@@ -20,7 +20,7 @@ class CreateUsers < ActiveRecord::Migration
         
       t.timestamps
     end
-    add_index :users, :login
+    add_index :users, :email
     add_index :users, :persistence_token
     
   end
